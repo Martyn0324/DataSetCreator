391c5727f0c1d0264da091d20f34676197d8e57f
Martyn <giovanni.ab@outlook.com>
2022-09-19 08:52:24 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import numpy as np
 from PIL import Image
+from scipy.io import wavfile
 
 class DatasetCreator():
     '''A shortcut to create your own datasets. Developed with focus on Machine Learning, specially Neural Networks, but perhaps might be useful
@@ -51,13 +52,59 @@ class DatasetCreator():
         
         return pics
     
-    def audio():
-        # TODO - Maybe if someday I learn how to create an audio dataset...
-        raise NotImplementedError
+    def audio(path):
+        # I think this can follow the same template as image...at least it worked when I tried...
+        '''
+        Returns an array with shape (n_samples, audio_data, n_channels). Created based on images dataset.
+        Make sure that your audio files are in .wav format, all with the same channel(1, Mono, or 2, stereo) and they all have the same Sample Rate
+        
+        path: Path where you got the audio saved.
+        '''
+        audios = []
+        
+        for directory, _, files in os.walk(path):
+            for file in files:
+                audios.append(directory+'/'+file)
+        
+        # Removing any files that aren't .wav audio
+        audios = [i for i in audios if ".wav" in i]
+        
+        audio = []
+        for i in audios:
+            sample_rate, data = wavfile_read(i)
+            data.append(audio)
+            
+        audio = np.array(audio)
+        
+        try:
+            audio = np.stack(audio, axis=0)
+        
+        except ValueError: # The audios have different shapes. Padding in order to make sure they all have the same shape.
+            max_shape = 0
+            for i in audio:
+                if i.shape[0] > max_shape
+                max_shape = i.shape[0]
+            
+            for i in audio: # There must be a more efficient way to do this...
+                if i.shape[0] < max_shape:
+                    pad = max_shape-i.shape[0]
+                    i = np.pad(i, [(0, pad), (0,0)]) # Now all audios will have the same shape(AKA duration), only with some silence after the sound itself.
+            
+            audio = np.stack(audio, axis=0)
+            
+        
+        print(f'dataset shape: {audio.shape}\nSample Rate: {sample_rate}')
+        
+        return audio, sample_rate
     
     def preprocess(dataset, type='image'):
-        '''Normalizes the dataset, returning it with values between -1 and 1.
-        If you want to plot the images, you need to denormalize it using deprocess()'''
+        '''
+        Normalizes the dataset, returning it with values between -1 and 1.
+        
+        If you want to plot the images, you need to denormalize it using deprocess()
+        
+        If you want to listen to the audio, you need to denormalize it using deprocess()
+        '''
         if type in ('image', 'img'):
             dataset = dataset.astype('float32') # Floats are necessary, otherwise the normalization will generate pixels with 0 values.
             if dataset.shape[0] > 3000:
@@ -72,10 +119,15 @@ class DatasetCreator():
             return dataset
         
         elif type == 'audio':
-            raise NotImplementedError
+            dataset = dataset.astype("float64")
+            dataset = (dataset - np.min(dataset))*2.0 / (np.max(dataset) - np.min(dataset))-1.0
+            
+            print(f"Dataset type: {type(dataset)}, {dataset.dtype}")
+            
+            return dataset
 
-    def deprocess(dataset, type='image', normalized=True):
-        '''Returns the dataset in a way that it can be plotted(images) or even denormalized(normalized=False)'''
+    def deprocess(dataset, type='image', normalized=True, preprocessed_data = None):
+        '''Returns the dataset in a way that it can be plotted(images)/listened(audio) or even denormalized(normalized=False)'''
         if type in ('image', 'img'):
             if normalized:
                 dataset = (dataset+1.0)*0.5
@@ -88,7 +140,15 @@ class DatasetCreator():
             return dataset
         
         elif type == 'audio':
-            raise NotImplementedError
+            if preprocessed_data is None:
+                raise ValueError("For audios, you need to pass the preprocessed data as argument")
+                
+            dataset = ((dataset+1.0)*(np.max(preprocessed_data)-np.min(preprocessed_data))*0.5) + np.min(preprocessed_data)
+            dataset = dataset.astype(np.(preprocessed_data.dtype))
+            
+            print("dataset values are now back to their normal range")
+            
+            return dataset
 
         
     
